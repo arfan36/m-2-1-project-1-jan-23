ed325078f41078fdb0c3fa8f06ffc688a5d0cc9e
Arfanur Rahman <arfanur36@gmail.com>
2023-02-14 21:49:37 +0600

    m-3 05 Handling remove functionality.

--- src/component/ProductCard.js
+++ src/component/ProductCard.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { BiListPlus } from "react-icons/bi";
-import { addToCart } from "../redux/actionCreators/productActions";
+import { RiDeleteBin2Line } from "react-icons/ri";
+
+import {
+	addToCart,
+	removeFromCart,
+} from "../redux/actionCreators/productActions";
+import { useLocation } from "react-router-dom";
 
 const ProductCard = ({
 	product,
 	product: { image, keyFeature, model, rating },
 }) => {
 	const dispatch = useDispatch();
+	const { pathname } = useLocation();
 
 	return (
 		<div className="m-4">
@@ -29,19 +36,34 @@ const ProductCard = ({
 							{feature}
 						</p>
 					))}
-					<div className="flex gap-2">
-						<button
-							onClick={() => dispatch(addToCart(product))}
-							className="btn btn-primary btn-sm w-5/6"
-						>
-							Buy Now
-						</button>
-						<button
-							title="Add to wishlist"
-							className="btn btn-primary btn-sm rounded-full w-1/6"
-						>
-							<BiListPlus className="text-2xl" />
-						</button>
+					<div className="flex gap-4">
+						<div className="flex gap-2 w-5/6">
+							{pathname.includes("cart") && (
+								<button
+									onClick={() => dispatch(removeFromCart(product))}
+									className="btn btn-error hover:bg-red-600 text-white btn-sm rounded-full w-1/2 gap-2"
+								>
+									<span>Remove</span>
+									<RiDeleteBin2Line className="text-2xl" />
+								</button>
+							)}
+							{!pathname.includes("cart") && (
+								<button
+									onClick={() => dispatch(addToCart(product))}
+									className="btn btn-primary btn-sm rounded-full w-1/2"
+								>
+									Add to cart
+								</button>
+							)}
+						</div>
+						{!pathname.includes("cart") && (
+							<button
+								title="Add to wishlist"
+								className="btn btn-primary btn-sm rounded-full w-1/6"
+							>
+								<BiListPlus className="text-2xl" />
+							</button>
+						)}
 					</div>
 				</div>
 			</div>
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,36 +1,20 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import ProductCard from "../component/ProductCard";
-import { useProducts } from "../context/ProductProvider";
 
 const Cart = () => {
-	const {
-		state: { cart, loading, error },
-	} = useProducts();
-
-	let content;
-	if (loading) {
-		content = <p>Loading</p>;
-	}
-	if (error) {
-		content = <p>Something went wrong</p>;
-	}
-	if (!loading && !error && cart.length === 0) {
-		content = <p>Nothing to show, product list is empty</p>;
-	}
-	if (!loading && !error && cart.length) {
-		content = cart.map((product) => (
-			<ProductCard
-				key={product._id}
-				product={product}
-			/>
-		));
-	}
+	const cart = useSelector((state) => state.cart);
 
 	return (
 		<>
 			<h2 className="text-center text-2xl mt-4">Cart: {cart.length}</h2>
 			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center ">
-				{content}
+				{cart.map((product) => (
+					<ProductCard
+						key={product._id}
+						product={product}
+					></ProductCard>
+				))}
 			</div>
 		</>
 	);
